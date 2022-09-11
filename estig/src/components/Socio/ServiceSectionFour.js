e99f72152971ee65cda142e52277948497449a50
Gaofeng Yin <Gaofeng.Yin@ctw.bmwgroup.com>
2022-09-11 17:48:45 +0100

    socio form finished

--- estig/src/components/Socio/ServiceSectionFour.js
+++ estig/src/components/Socio/ServiceSectionFour.js
@@ -1,46 +1,5 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
 
-const options = [
-    '[CTeSP] Análises Químicas e Biológicas', 
-    '[CTeSP] Automação, Robótica e Eletrónica Industrial',
-    '[CTeSP] Cibersegurança',
-    '[CTeSP] Contabilidade',
-    '[CTeSP] Construção Civil',
-    '[CTeSP] Desenvolvimento de Software',
-    '[CTeSP] Energias Renováveis e Infraestruturas Elétricas e de Telecomunicações',
-    '[CTeSP] Energias Renováveis e Instalações Elétricas',
-    '[CTeSP] Gestão de Operações e Logísitica',
-    '[CTeSP] Prospeção Mineral e Geotécnica',
-    '[CTeSP] Qualidade e Tratamento de Água e Efluentes',
-    '[CTeSP] Tecnologia Mecânica e Veículos',
-    'Contabilidade',
-    'Engenharia Civil',
-    'Engenharia de Energias Renováveis',
-    'Engenharia Eletrónica e de Computadores',
-    'Engenharia Informática',
-    'Engenharia Mecânica',
-    'Engenharia Química',
-    'Gestão',
-    'Gestão de Negócios Internacionais',
-    'Informática de Gestão',
-    'Tecnologia Biomédica',
-    'Mestrado Tecnologia e Gestão Industrial',
-    'Mestrado Contabilidade e Finanças',
-    'Mestrado Empreendorismo e Inovação',
-    'Mestrado Engenharia da Construção',
-    'Mestrado Engenharia Industrial',
-    'Mestrado Engenharia Química',
-    'Mestrado Energias Renováveis e Eficiência Energética',
-    'Mestrado Gestão das Organizações',
-    'Mestrado Informática',
-    'Mestrado Inovação de Produtos e Processos',
-    'Mestrado Logística',
-    'Mestrado Tecnologia Biomédica'
-  ];
-  const defaultOption = options[0];
 class ServiceFour extends Component {
     setType(){
         this.type = 'date';
@@ -60,7 +19,7 @@ class ServiceFour extends Component {
                     </div>
                     <div className="col-xxl-7 col-xl-6 col-lg-6">
                         <div className="contact-form">
-                            <form action="https://mailthis.to/aeestig" id="contact-form" method="POST" encType="multipart/form-data">
+                            <form action="https://mailthis.to/aeee" id="contact-form" method="POST" encType="multipart/form-data">
                                 <div className="row">
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
                                         <input name="primeiro nome" type="text" placeholder="Primeiro Nome" required />
@@ -72,7 +31,7 @@ class ServiceFour extends Component {
                                         <input name="Email" type="email" placeholder="Email" required/>
                                     </div>
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="Telemóvel" type="number" placeholder="Telemóvel"/>
+                                        <input name="Telemovel" type="number" placeholder="Telemóvel"/>
                                     </div>
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
                                         <input name="Localidade" type="text" placeholder="Localidade" required/>
@@ -87,14 +46,50 @@ class ServiceFour extends Component {
                                         <input name="Pais" type="text" placeholder="País"/>
                                     </div>
                                     <div className="col-xxl-12 col-xl-12 col-lg-12 mb-20">
-                                        <Dropdown className='dropdown' options={options} onChange={this._onSelect}  placeholder="Seleciona o teu curso" />
+                                        <select className="dropdown" name="Curso" placeholder="Seleciona o teu curso">
+                                            <option value="[CTeSP] Análises Químicas e Biológicas">[CTeSP] Análises Químicas e Biológicas</option>
+                                            <option value="[CTeSP] Automação, Robótica e Eletrónica Industrial">[CTeSP] Automação, Robótica e Eletrónica Industrial</option>
+                                            <option value="[CTeSP] Cibersegurança">[CTeSP] Cibersegurança</option>
+                                            <option value="[CTeSP] Contabilidade">[CTeSP] Contabilidade</option>
+                                            <option value="[CTeSP] Construção Civil">[CTeSP] Construção Civil</option>
+                                            <option value="[CTeSP] Desenvolvimento de Software">[CTeSP] Desenvolvimento de Software</option>
+                                            <option value="[CTeSP] Energias Renováveis e Infraestruturas Elétricas e de Telecomunicações">[CTeSP] Energias Renováveis e Infraestruturas Elétricas e de Telecomunicações</option>
+                                            <option value="[CTeSP] Energias Renováveis e Instalações Elétricas">[CTeSP] Energias Renováveis e Instalações Elétricas</option>
+                                            <option value="[CTeSP] Gestão de Operações e Logísitica">[CTeSP] Gestão de Operações e Logísitica</option>
+                                            <option value="[CTeSP] Prospeção Mineral e Geotécnica">[CTeSP] Prospeção Mineral e Geotécnica</option>
+                                            <option value="[CTeSP] Qualidade e Tratamento de Água e Efluentes">[CTeSP] Qualidade e Tratamento de Água e Efluentes</option>
+                                            <option value="[CTeSP] Tecnologia Mecânica e Veículos">[CTeSP] Tecnologia Mecânica e Veículos</option>
+                                            <option value="Contabilidade">Contabilidade</option>
+                                            <option value="Engenharia Civil">Engenharia Civil</option>
+                                            <option value="Engenharia de Energias Renováveis">Engenharia de Energias Renováveis</option>
+                                            <option value="Engenharia Eletrónica e de Computadores">Engenharia Eletrónica e de Computadores</option>
+                                            <option value="Engenharia Informática">Engenharia Informática</option>
+                                            <option value="Engenharia Mecânica">Engenharia Mecânica</option>
+                                            <option value="Engenharia Química">Engenharia Química</option>
+                                            <option value="Gestão">Gestão</option>
+                                            <option value="Gestão de Negócios Internacionais">Gestão de Negócios Internacionais</option>
+                                            <option value="Informática de Gestão">Informática de Gestão</option>
+                                            <option value="Tecnologia Biomédica">Tecnologia Biomédica</option>
+                                            <option value="Mestrado Tecnologia e Gestão Industrial">Mestrado Tecnologia e Gestão Industrial</option>
+                                            <option value="Mestrado Contabilidade e Finanças">Mestrado Contabilidade e Finanças</option>
+                                            <option value="Mestrado Empreendorismo e Inovação">Mestrado Empreendorismo e Inovação</option>
+                                            <option value="Mestrado Engenharia da Construção">Mestrado Engenharia da Construção</option>
+                                            <option value="Mestrado Engenharia Industrial">Mestrado Engenharia Industrial</option>
+                                            <option value="Mestrado Engenharia Química">Mestrado Engenharia Química</option>
+                                            <option value="Mestrado Energias Renováveis e Eficiência Energética">Mestrado Energias Renováveis e Eficiência Energética</option>
+                                            <option value="Mestrado Gestão das Organizações">Mestrado Gestão das Organizações</option>
+                                            <option value="Mestrado Informática">Mestrado Informática</option>
+                                            <option value="Mestrado Inovação de Produtos e Processos">Mestrado Inovação de Produtos e Processos</option>
+                                            <option value="Mestrado Logística">Mestrado Logística</option>
+                                            <option value="Mestrado Tecnologia Biomédica">Mestrado Tecnologia Biomédica</option>
+                                        </select>
                                     </div>
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input id="date" name="Data Nascimento" type="text" placeholder="Data Nascimento (13/07/96)"/>
+                                        <input name="Data Nascimento" type="text" placeholder="Data Nascimento (13/07/96)"/>
                                     </div>
                                     
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="Telemóvel" type="number" placeholder="Número Mecanográfico"/>
+                                        <input name="Numero mecanografico" type="number" placeholder="Número Mecanográfico"/>
                                     </div>
                                     <div className="col-xxl-12 col-xl-12 mb-20">
                                         <button type="submit" className="theme-btn border-btn">Aderir</button>
