8266ea4f5c270955ab218616e502e19aa4e596b5
Gaofeng Yin <Gaofeng.Yin@ctw.bmwgroup.com>
2022-09-11 16:18:35 +0100

    socio forms

--- estig/src/components/Socio/ServiceSectionFour.js
+++ estig/src/components/Socio/ServiceSectionFour.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
-
+const options = [
+    '[CTeSP] Análises Químicas e Biológicas', 
+    '[CTeSP] Automação, Robótica e Eletrónica Industrial',
+    '[CTeSP] Cibersegurança',
+    '[CTeSP] Contabilidade',
+    '[CTeSP] Construção Civil',
+    '[CTeSP] Desenvolvimento de Software',
+    '[CTeSP] Energias Renováveis e Infraestruturas Elétricas e de Telecomunicações',
+    '[CTeSP] Energias Renováveis e Instalações Elétricas',
+    '[CTeSP] Gestão de Operações e Logísitica',
+    '[CTeSP] Prospeção Mineral e Geotécnica',
+    '[CTeSP] Qualidade e Tratamento de Água e Efluentes',
+    '[CTeSP] Tecnologia Mecânica e Veículos',
+    'Contabilidade',
+    'Engenharia Civil',
+    'Engenharia de Energias Renováveis',
+    'Engenharia Eletrónica e de Computadores',
+    'Engenharia Informática',
+    'Engenharia Mecânica',
+    'Engenharia Química',
+    'Gestão',
+    'Gestão de Negócios Internacionais',
+    'Informática de Gestão',
+    'Tecnologia Biomédica',
+    'Mestrado Tecnologia e Gestão Industrial',
+    'Mestrado Contabilidade e Finanças',
+    'Mestrado Empreendorismo e Inovação',
+    'Mestrado Engenharia da Construção',
+    'Mestrado Engenharia Industrial',
+    'Mestrado Engenharia Química',
+    'Mestrado Energias Renováveis e Eficiência Energética',
+    'Mestrado Gestão das Organizações',
+    'Mestrado Informática',
+    'Mestrado Inovação de Produtos e Processos',
+    'Mestrado Logística',
+    'Mestrado Tecnologia Biomédica'
+  ];
+  const defaultOption = options[0];
 class ServiceFour extends Component {
+    setType(){
+        this.type = 'date';
+    }
 
     render() {
 
@@ -21,7 +63,10 @@ class ServiceFour extends Component {
                             <form action="https://mailthis.to/aeestig" id="contact-form" method="POST" encType="multipart/form-data">
                                 <div className="row">
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="Nome" type="text" placeholder="Nome Completo" required />
+                                        <input name="primeiro nome" type="text" placeholder="Primeiro Nome" required />
+                                    </div>
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input name="ultimo nome" type="text" placeholder="Último Nome" required />
                                     </div>
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
                                         <input name="Email" type="email" placeholder="Email" required/>
@@ -30,7 +75,26 @@ class ServiceFour extends Component {
                                         <input name="Telemóvel" type="number" placeholder="Telemóvel"/>
                                     </div>
                                     <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
-                                        <input name="Assunto" type="text" placeholder="Assunto" required/>
+                                        <input name="Localidade" type="text" placeholder="Localidade" required/>
+                                    </div>
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input name="Codigo Postal" type="number" placeholder="Código Postal"/>
+                                    </div>
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input name="NIF" type="number" placeholder="Nº Identificação Fiscal" required/>
+                                    </div>
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input name="Pais" type="text" placeholder="País"/>
+                                    </div>
+                                    <div className="col-xxl-12 col-xl-12 col-lg-12 mb-20">
+                                        <Dropdown className='dropdown' options={options} onChange={this._onSelect}  placeholder="Seleciona o teu curso" />
+                                    </div>
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input id="date" name="Data Nascimento" type="text" placeholder="Data Nascimento (13/07/96)"/>
+                                    </div>
+                                    
+                                    <div className="col-xxl-6 col-xl-6 col-lg-6 mb-20">
+                                        <input name="Telemóvel" type="number" placeholder="Número Mecanográfico"/>
                                     </div>
                                     <div className="col-xxl-12 col-xl-12 mb-20">
                                         <button type="submit" className="theme-btn border-btn">Aderir</button>
