80cc438fc8fcf9f9ac947be4e9f60c0e02ba55ba
Gaofeng Yin <Gaofeng.Yin@ctw.bmwgroup.com>
2022-09-13 19:23:26 +0100

    socio done

--- estig/src/components/Socio/SponsorAccordionSection.js
+++ estig/src/components/Socio/SponsorAccordionSection.js
@@ -8,6 +8,7 @@ class FaqAccordion extends Component {
         <section className="faq-area pt-120 pb-100 bg-grey fix">
             <div className="container z-index">
                 <div className="row">
+                <h2 className="section-title mb-35">Vantagens</h2>
                     <div className="col-lg-6">
                         <div className="faq-wrapper">
                             <div className="choose-right" data-aos="fade-left" data-aos-duration="1000">
@@ -15,7 +16,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                             <h2 className="accordion-header" id="headingOne">
                                                 <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                                    Bar Académico
+                                                    Bar Académico 🍺
                                                 </button>
                                             </h2>
                                             <div id="collapseOne" className="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
@@ -27,7 +28,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingTwo">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                Under Bar
+                                                Under Bar 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
@@ -39,7 +40,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingThree">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                                Urban Bar
+                                                Urban Bar 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
@@ -51,7 +52,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingFour">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                                Plano B
+                                                Plano B 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseFour" className="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
@@ -63,7 +64,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingFive">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                                Boite
+                                                Boite 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseFive" className="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
@@ -75,7 +76,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingFive5">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive5" aria-expanded="false" aria-controls="collapseFive5">
-                                                Gulden Draak
+                                                Gulden Draak 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseFive5" className="accordion-collapse collapse" aria-labelledby="headingFive5" data-bs-parent="#accordionExample">
@@ -95,7 +96,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingSix">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="true" aria-controls="collapseSix">
-                                                Mercado Club
+                                                Mercado Club 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseSix" className="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample2">
@@ -107,7 +108,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingSeven">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
-                                                Mac Donalds
+                                                Mac Donalds 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseSeven" className="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample2">
@@ -119,7 +120,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingEight">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
-                                                Metro Bar
+                                                Metro Bar 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseEight" className="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample2">
@@ -131,7 +132,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingNine">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
-                                                Espaço Caffe
+                                                Espaço Caffe 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseNine" className="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample2">
@@ -143,7 +144,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingTen">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
-                                                Dux
+                                                Dux 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseTen" className="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#accordionExample2">
@@ -155,7 +156,7 @@ class FaqAccordion extends Component {
                                     <div className="accordion-item">
                                         <h2 className="accordion-header" id="headingEleven">
                                             <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
-                                                Moda Club
+                                                Moda Club 🍺
                                             </button>
                                         </h2>
                                         <div id="collapseEleven" className="accordion-collapse collapse" aria-labelledby="headingEleven" data-bs-parent="#accordionExample2">
