dd84a3e036c6989497a264065d06997cac9d908f
Jorge Rodrigues <jorge_rodrigues_7@hotmail.com>
2022-09-14 01:33:10 +0100

    textos

--- estig/src/components/About/MissionSection.js
+++ estig/src/components/About/MissionSection.js
@@ -12,15 +12,11 @@ class Mission extends Component {
                     <div className="col-xl-6 col-lg-6">
                         <div className="kintro-text mr-xs-0 mr-md-0 mr-lg-0 mr-70 mb-30" data-aos="fade-left" data-aos-duration="1000">
                             <div className="section-title-wrapper mb-25">
-                                <h5 className="section-subtitle mb-20">get to know us</h5>
-                                <h2 className="section-title mb-35">Our Mission is to
-                                    become the best
-                                    agency</h2>
-                                <p>Lorem Ipsum is simply dummy text of the printing available in the market for free uses.</p>
+                                <h5 className="section-subtitle mb-20">Fica a conhecer-nos um pouco melhor...</h5>
+                                <h2 className="section-title mb-35">AEESTiG</h2>
+                                <p>A AEESTiG é o órgão de representação dos alunos da Escola Superior de Tecnologia e Gestão de Bragança. Já com mais de 3 décadas de história, mas com o objetivo de sempre fazer mais pela ESTiG e pelos seus estudantes.</p>
                             </div>
-                            <p className="mb-45">Lorem ipsum dolor sit amet nsectetur cing elit. Suspe ndisse suscipit sagittis leo
-                                sit met entum is not estibulum dignissim posuere cubilia durae. Leo sit met entum cubilia crae.
-                                but also the leap into electronic typesetting. 
+                            <p className="mb-45">A ação desta associação distingue-se pela abrangência das áreas de intervenção, tendo como principal objetivo a proximidade com os alunos. Ao longo destes anos, esta associação de estudantes tem contribuído de forma notável no acompanhamento e integração dos alunos, estando sempre atenta a questões relativas à qualidade de ensino.
                             </p>
                         </div>
                     </div>
--- estig/src/components/Home/AboutSection.js
+++ estig/src/components/Home/AboutSection.js
@@ -27,31 +27,31 @@ class About extends Component {
                         <div className="col-lg-6">
                             <div className="kabout mb-50" data-aos="zoom-out-down" data-swiper-autoplay="10000">
                                 <div className="section-title-wrapper mb-45">
-                                    <h5 className="section-subtitle mb-20">get to know us</h5>
-                                    <h2 className="section-title mb-35">Bem-Vindo <br/>agency</h2>
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the when an unknown was popularised.</p>
+                                    <h5 className="section-subtitle mb-20">A escola...</h5>
+                                    <h2 className="section-title mb-35">ESTiG <br/></h2>
+                                    <p>A Escola Superior de Tecnologia e Gestão de Bragança (ESTiG) é uma das cinco escolas que integram o Instituto Politécnico de Bragança e inclui na sua oferta formativa cursos nas áreas das engenharias e das ciências empresariais. A ESTiG dispõe de instalações no Campus de Santa Apolónia que engloba uma área total de 14000 m2.</p>
                                 </div>
                                 <div className="row">
                                     <div className="col-sm-6">
                                         <div className="kabout-service mb-30">
-                                            <h4 className="kabout-service-title">The Best Services</h4>
-                                            <p>Lorem ipsum dolor sited amet, consectetur notted.</p>
+                                            <h4 className="kabout-service-title">Diversidade e qualidade de ensino</h4>
+                                            <p>6 cursos de especialização tecnológica, 11 licenciaturas, 9 mestrados</p>
                                         </div>
                                     </div>
                                     <div className="col-sm-6">
                                         <div className="kabout-service mb-30">
-                                            <h4 className="kabout-service-title">Expert Designers</h4>
-                                            <p>Lorem ipsum dolor sited amet, consectetur notted.</p>
+                                            <h4 className="kabout-service-title">Qualidade e baixo custo de vida</h4>
+                                            <p>Bragança é uma das cidades europeias com melhor qualidade de vida, com um baixo custo de vida</p>
                                         </div>
                                     </div>
                                 </div>
                                 <ul className="mt-20 mb-55">
-                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> Entum estibulum dignissim posuere.</li>
-                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> If you are going to use a passage.</li>
-                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> Lorem Ipsum generators on the tend to repeat.</li>
+                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> Qualidade das instalações e equipamentos</li>
+                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> Interculturalidade e Internacionalização</li>
+                                    <li><i><FontAwesomeIcon icon={['fas', 'check']} /></i> Ação social e apoio aos estudantes</li>
                                 </ul>
                                 <div className="kabout-btn">
-                                    <Link href="/service"><a className="theme-btn border-btn">Discover more</a></Link>
+                                    <Link href="https://estig.ipb.pt/"><a className="theme-btn border-btn">Conhece a Escola</a></Link>
                                 </div>
                             </div>
                         </div>
